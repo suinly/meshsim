acce7fea467f65ee1869362b42bf20b0a7802bc5
Daniel Flow <thedanielflow@gmail.com>
2025-12-08 14:51:28 +0300

    feat(app): integrate Yandex Metrika for enhanced analytics tracking

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,18 @@ useHead({
   link: [{ rel: 'icon', href: '/favicon.ico' }],
   htmlAttrs: {
     lang: 'ru'
-  }
+  },
+  script: [
+    {
+      children: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');ym(105731036,'init',{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});`,
+      type: 'text/javascript'
+    }
+  ],
+  noscript: [
+    {
+      children: '<div><img src="https://mc.yandex.ru/watch/105731036" style="position:absolute; left:-9999px;" alt="" /></div>'
+    }
+  ]
 })
 
 const title = 'Симулятор Mesh-сети'
