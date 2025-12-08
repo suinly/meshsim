a9f6b79309aef20ff076fee3f8342ac52d4967b4
Daniel Flow <thedanielflow@gmail.com>
2025-12-08 15:00:02 +0300

    feat(app): migrate Yandex Metrika integration to a dedicated plugin

---
+++ app/plugins/yandex-metrika.client.ts
@@ -0,0 +1,38 @@
+export default defineNuxtPlugin(() => {
+  if (import.meta.client) {
+    // Инициализируем Яндекс Метрику
+    (function (m, e, t, r, i, k, a) {
+      m[i] =
+        m[i] ||
+        function () {
+          (m[i].a = m[i].a || []).push(arguments);
+        };
+      m[i].l = 1 * new Date();
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) {
+          return;
+        }
+      }
+      ((k = e.createElement(t)),
+        (a = e.getElementsByTagName(t)[0]),
+        (k.async = 1),
+        (k.src = r),
+        a.parentNode.insertBefore(k, a));
+    })(
+      window,
+      document,
+      "script",
+      "https://mc.yandex.ru/metrika/tag.js?id=105731036",
+      "ym",
+    );
+
+    window.ym(105731036, "init", {
+      ssr: true,
+      webvisor: true,
+      clickmap: true,
+      ecommerce: "dataLayer",
+      accurateTrackBounce: true,
+      trackLinks: true,
+    });
+  }
+});
