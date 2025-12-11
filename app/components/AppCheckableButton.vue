c5cfaede4a641a1ac4e0b191c6203217a9313cf0
Daniel Flow <thedanielflow@gmail.com>
2025-12-11 20:11:15 +0300

    feat(UI): introduce AppCheckableButton component for enhanced interactivity

---
+++ app/components/AppCheckableButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <UTooltip
+    :delay-duration="0"
+    :text="tooltip"
+    :disabled="!tooltip"
+    :content="{ side: 'top' }"
+  >
+    <UButton
+      :icon="icon"
+      :color="checked ? color : 'neutral'"
+      :variant="checked ? 'solid' : 'soft'"
+    />
+  </UTooltip>
+</template>
+
+<script setup lang="ts">
+const {
+  color = "primary",
+  icon,
+  checked = false,
+  tooltip,
+} = defineProps<{
+  icon: string;
+  color?:
+    | "error"
+    | "primary"
+    | "secondary"
+    | "success"
+    | "info"
+    | "warning"
+    | "neutral";
+  checked?: boolean;
+  tooltip?: string;
+}>();
+</script>
