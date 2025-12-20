7e944139944242fc77cefb25f3dffd20ffa29705
Daniel Flow <thedanielflow@gmail.com>
2025-12-20 21:02:14 +0300

    feat(logs): enhance log viewer display with improved styling and formatting

--- app/components/MeshLogViewer.vue
+++ app/components/MeshLogViewer.vue
@@ -3,23 +3,22 @@
     <div
       v-for="(log, index) in logger.storage"
       :key="index"
-      class="font-mono text-xs"
+      class="font-mono text-xs flex gap-2"
+      :class="{
+        'text-red-400': log.type === LogEntityType.ERROR,
+        'text-yellow-400': log.type === LogEntityType.WARNING,
+        'text-green-400': log.type === LogEntityType.SUCCESS,
+      }"
     >
+      [{{ formatDate(log.createdAt) }}]
       <span
-        :class="{
-          'text-red-400': log.type === LogEntityType.ERROR,
-          'text-yellow-400': log.type === LogEntityType.WARNING,
-          'text-green-400': log.type === LogEntityType.SUCCESS,
-        }"
+        v-if="log.node"
+        class="rounded-full w-4 h-4 inline-block text-black text-center text-[10px]/4 font-bold"
+        :class="getColorByNode(log.node)"
       >
-        [{{ formatDate(log.createdAt) }}]
+        {{ log.node.id }}
       </span>
-      <span v-if="log.node" :class="getColorByNode(log.node)">
-        [{{ log.node.id }}]
-      </span>
-      <span>
-        {{ log.message }}
-      </span>
+      <span>{{ log.message }}</span>
     </div>
   </div>
 </template>
@@ -48,13 +47,14 @@ const formatDate = (date: Date): string => {
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  return `${hours}:${minutes}:${seconds}`;
+  const milliseconds = String(date.getMilliseconds()).padStart(3, "0");
+  return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 };
 
 const getColorByNode = (node: BaseNode): string => {
-  if (node.role === NodeRole.CLIENT) return "text-yellow-400";
-  if (node.role === NodeRole.CLIENT_MUTE) return "text-blue-400";
-  if (node.role === NodeRole.ROUTER) return "text-red-400";
+  if (node.role === NodeRole.CLIENT) return "bg-yellow-400";
+  if (node.role === NodeRole.CLIENT_MUTE) return "bg-blue-400";
+  if (node.role === NodeRole.ROUTER) return "bg-red-400";
 
   return "";
 };
