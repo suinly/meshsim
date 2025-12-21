dee1a3e5b6373e0bbeb80085907ce0a606a33afe
Daniel Flow <thedanielflow@gmail.com>
2025-12-21 13:23:18 +0300

    fix(config): update module dependencies to latest versions and improve formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxtjs/leaflet'],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxtjs/leaflet"],
 
   ssr: false,
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
   app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/'
+    baseURL: process.env.NUXT_APP_BASE_URL || "/",
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
-  compatibilityDate: '2025-01-15',
+  compatibilityDate: "2025-01-15",
+
+  icon: {
+    clientBundle: {
+      scan: true,
+      sizeLimitKb: 512,
+    },
+  },
 
   nitro: {
     prerender: {
       failOnError: false,
       crawlLinks: false,
-      routes: ['/']
-    }
+      routes: ["/"],
+    },
   },
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
-})
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
+  },
+});
