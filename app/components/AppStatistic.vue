261bb0c2b99473bd1624c636815557d7f96e73cd
Daniel Flow <thedanielflow@gmail.com>
2025-12-27 17:47:55 +0200

    feat(statistics): enhance node reception display with coverage information

--- app/components/AppStatistic.vue
+++ app/components/AppStatistic.vue
@@ -1,16 +1,10 @@
 <template>
   <div v-if="lastPacketId > 0">
     <p>Пакет #{{ lastPacketId }}</p>
-    <p class="mt-3 text-sm text-gray-600 dark:text-gray-400">
-      Приняло узлов: {{ receivedNodes.length }}/{{
-        simulator.nodes.length - 1
-      }}
-      ({{
-        ((receivedNodes.length / (simulator.nodes.length - 1)) * 100).toFixed(
-          2,
-        )
-      }}%)
-    </p>
+    <div class="mt-3 text-sm text-gray-600 dark:text-gray-400">
+      <p>Приняло узлов: {{ receivedNodesCount }}/{{ totalNodesCount }}</p>
+      <p>Покрытие: {{ percentOfReceived.toFixed(2) }}%</p>
+    </div>
   </div>
 </template>
 
@@ -21,12 +15,22 @@ const lastPacketId = computed(() => {
   return simulator.packetCount;
 });
 
-const receivedNodes = computed(() => {
-  return simulator.nodes.filter((node) =>
-    node.receivedPackets.find(
-      (packet) =>
-        packet.id === lastPacketId.value && packet.sourceId !== node.id,
-    ),
+const totalNodesCount = computed(() => {
+  return simulator.nodes.length - 1;
+});
+
+const receivedNodesCount = computed(() => {
+  return (
+    simulator.nodes.filter((node) =>
+      node.receivedPackets.find(
+        (packet) =>
+          packet.id === lastPacketId.value && packet.sourceId !== node.id,
+      ),
+    ).length || 0
   );
+});
+
+const percentOfReceived = computed(() => {
+  return (receivedNodesCount.value / totalNodesCount.value) * 100;
 });
 </script>
