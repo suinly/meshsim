cdd015cd4b2fecf8f9c3424539389c3ce6a4812d
Daniel Flow <thedanielflow@gmail.com>
2025-12-31 13:23:42 +0200

    feat(settings): add default height slider for new nodes in AppSettings.vue

--- app/components/AppSettings.vue
+++ app/components/AppSettings.vue
@@ -1,7 +1,23 @@
 <template>
   <UForm :schema="schema" :state="state">
-    <UFormField label="Лимит прыжков для новых узлов" name="hopLimit">
-      <UInputNumber v-model="state.hopLimit" />
+    <UFormField name="hopLimit">
+      <template #label
+        >Лимит прыжков для новых узлов ({{ state.hopLimit }})</template
+      >
+      <USlider v-model="state.hopLimit" :min="1" :max="7" :step="1" />
+    </UFormField>
+
+    <UFormField name="defaultHeight" class="mt-2 mb-2">
+      <template #label
+        >Высота новых узлов ({{ state.defaultHeight }}м)</template
+      >
+      <USlider
+        v-model="state.defaultHeight"
+        :min="0"
+        :max="200"
+        :step="1"
+        class="mt-2"
+      />
     </UFormField>
   </UForm>
 </template>
@@ -11,14 +27,16 @@ import * as z from "zod";
 
 const schema = z.object({
   hopLimit: z.number().positive("Не может быть отрицательным").max(7),
+  defaultHeight: z.number().positive("Не может быть отрицательным").max(200),
 });
 
 type Schema = z.output<typeof schema>;
 
-const { hopLimit } = useSimulatorSettings();
+const { hopLimit, defaultHeight } = useSimulatorSettings();
 
 const state = reactive<Partial<Schema>>({
   hopLimit: hopLimit.value,
+  defaultHeight: defaultHeight.value,
 });
 
 watch(
@@ -26,6 +44,15 @@ watch(
   (newValue) => {
     if (newValue !== undefined) {
       hopLimit.value = newValue;
+    }
+  },
+);
+
+watch(
+  () => state.defaultHeight,
+  (newValue) => {
+    if (newValue !== undefined) {
+      defaultHeight.value = newValue;
     }
   },
 );
--- app/composables/useSimulatorSettings.ts
+++ app/composables/useSimulatorSettings.ts
@@ -3,9 +3,11 @@ import { NodeRole } from "~/simulator/NodeRole";
 export const useSimulatorSettings = () => {
   const hopLimit = useState("simulator-hop-limit", () => 3);
   const defaultRole = useState("simulator-default-role", () => NodeRole.CLIENT);
+  const defaultHeight = useState("simulator-default-height", () => 0);
 
   return {
     hopLimit,
     defaultRole,
+    defaultHeight,
   };
 };
